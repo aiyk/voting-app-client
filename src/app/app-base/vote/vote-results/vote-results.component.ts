da23c90f90f997d44d00244f205779ee32ad209d
Ikechukwu Ekwe <iekwe@verifynigeria.com>
2019-09-20 19:54:59 +0100

    vote in place

---
+++ src/app/app-base/vote/vote-results/vote-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-results',
+  templateUrl: './vote-results.component.html',
+  styleUrls: ['./vote-results.component.scss']
+})
+export class VoteResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
