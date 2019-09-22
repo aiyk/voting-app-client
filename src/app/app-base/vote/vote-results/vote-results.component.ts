50916134402731cc8f7fa528dc23455ecb8ecaba
Ikechukwu Ekwe <iekwe@verifynigeria.com>
2019-09-22 03:23:43 +0100

    results fixed

--- src/app/app-base/vote/vote-results/vote-results.component.ts
+++ src/app/app-base/vote/vote-results/vote-results.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Election } from '../../../_models/election';
+
+import { AuthService } from '../../../services/auth.service';
+import { ElectionService} from '../../../services/election.service';
+import { PartyService} from '../../../services/party.service';
+import { ActivePageService} from '../../../services/active-page.service';
+
 
 @Component({
   selector: 'app-vote-results',
@@ -7,9 +17,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteResultsComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  elections: Election[];
+  submitted = false;
+  returnUrl: string;
+  error = '';
 
+  id: string;
+  initData: any = {};
+  compiledResult = [];
+
+  pgData = {
+    title: 'Vote Results',
+    button: {
+      title: 'Initialize',
+      route: 'vote'
+    }
+  };
+
+  constructor(
+    private electionService: ElectionService,
+    private partyService: PartyService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private pageData: ActivePageService) { }
+
+  clickItemIndex: number;
   ngOnInit() {
+    this.loading = false;
+
+    this.loadElections();
+
+    this.pageData.changePageData(this.pgData);
+    this.returnUrl = '/';
   }
+
+  loadElections(){
+    this.electionService.getAll().subscribe(elections => {
+      if(elections){
+        this.loading = false;
+        this.elections = elections.result;
+
+        this.partyService.getAll().subscribe(party => {
+          party.result.map(item => {
+            let partyname = item.partyname;
+            item.candidates.forEach( candidate =>{
+              if(this.compiledResult.length === 0){
+                this.compiledResult.push([ partyname, 0, candidate.electionname ]);
+              }
+              let notInList = true;
+              this.compiledResult.forEach(entry => {
+                if(entry[0] === partyname && entry[2] === candidate.electionname){
+                  notInList = false;
+                }
+              });
+              console.log(notInList);
+              if(notInList){
+                this.compiledResult.push([ partyname, 0, candidate.electionname ]);
+                notInList = false;
+              }
+            });
+            this.elections.forEach(item => {
+              item.votes.forEach(vote => {
+                this.partyService.getById(vote.party).subscribe(party => {
+                  // console.log(item);
+                  if(party){
+                    party.result.forEach( party_item => {
+                      // console.log(party_item);
+                      if(vote.party === party_item._id){
+                        vote.partyname = party_item.partyname;
+                      }
+                    });
+                  }
+                  this.compiledResult.forEach(partyResult => {
+                    if( partyResult[0] === vote.partyname && partyResult[2] === item.electionname){
+                      partyResult[1] += 1;
+                    }
+                  });
+                });
+              });
+              // console.log(this.compiledResult);
+            });
+          });
+        });
+
+      }
+    });
+  }
+
 
 }
